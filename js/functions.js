8973b8cdaf4ca6f105028d770c41015488583219
James Hooper <jameshooperdesigns@gmail.com>
2021-05-06 17:50:40 -0600

    Fix the padding in the navigation

--- js/functions.js
+++ js/functions.js
@@ -70,14 +70,3 @@ DONT CHANGE ANY CODE UNDER THIS LINE - DONT CHANGE ANY CODE UNDER THIS LINE - DO
 	  
 });
 
-
-$("#search-icon").click(function() {
-  $(".nav").toggleClass("search");
-  $(".nav").toggleClass("no-search");
-  $(".search-input").toggleClass("search-active");
-});
-
-$('.menu-toggle').click(function(){
-   $(".nav").toggleClass("mobile-nav");
-   $(this).toggleClass("is-active");
-});
